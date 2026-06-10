44ec08d6fbfeb3807f6b479c2931acfd6b0fd737
Kolawole Sodiya <kolawolesodiya@gmail.com>
2026-06-10 06:30:13 +0100

    Create course content registry

---
+++ src/content/courses/index.ts
@@ -0,0 +1,21 @@
+import beginnerFashionDesign from "./beginner-fashion-design";
+import professionalDressmaking from "./professional-dressmaking";
+import bridalMasterclass from "./bridal-masterclass";
+import menswearMasterclass from "./menswear-masterclass";
+import fashionBusinessBlueprint from "./fashion-business-blueprint";
+
+export const courses = [
+  beginnerFashionDesign,
+  professionalDressmaking,
+  bridalMasterclass,
+  menswearMasterclass,
+  fashionBusinessBlueprint,
+];
+
+export {
+  beginnerFashionDesign,
+  professionalDressmaking,
+  bridalMasterclass,
+  menswearMasterclass,
+  fashionBusinessBlueprint,
+};
