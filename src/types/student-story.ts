e0864dcf2da0574bbfa0141fecdcd3fc9b4d2114
Kolawole Sodiya <kolawolesodiya@gmail.com>
2026-06-10 11:36:17 +0100

    Enhance student story domain type

--- src/types/student-story.ts
+++ src/types/student-story.ts
@@ -1,15 +1,23 @@
 export interface StudentStory {
   id: string;
 
+  slug: string;
+
   name: string;
 
   image: string;
 
   location: string;
 
-  story: string;
+  course: string;
 
   achievement: string;
 
+  shortStory: string;
+
+  story: string;
+
   featured: boolean;
+
+  publishedAt: string;
 }
